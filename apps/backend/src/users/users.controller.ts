c7fcd386184f99996cb974b69d153584b95a7bc1
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-09-15 10:36:36 +0530

    Added updateUser

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,6 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UserInterface } from '@repo/dbschema';
-import type { CreateUserDto } from 'src/DTO/users.dto';
+import type { CreateUserDto, UpdateUserDto } from 'src/DTO/users.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -10,5 +17,14 @@ export class UsersController {
   async createUsers(@Body() input: CreateUserDto): Promise<UserInterface> {
     console.log(input);
     return this.userService.createUser(input);
+  }
+  @Patch('/update/:id')
+  updateUsers(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateInfo: UpdateUserDto,
+  ) {
+    console.log(id);
+    console.log(updateInfo);
+    return this.userService.updateUser(id, updateInfo);
   }
 }
